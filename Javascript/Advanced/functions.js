8ff991fe1eb56e2fbab65841321538b6a1d4864e
Aaron <51930273+Achan40@users.noreply.github.com>
2020-10-21 13:55:42 -0500

    finished JS basics and started JS advanced

---
+++ Javascript/Advanced/functions.js
@@ -0,0 +1,14 @@
+function hello() {
+    console.log("Hello World!");
+}
+
+function helloYou(name) {
+    console.log("Hello " + name);
+}
+
+//default arguements
+function formal(name="Aaron",title="Sir") {
+    console.log(title + " " + name);
+}
+
+// Not bothering too much with this, just needed to understand the syntax
--- Javascript/Basics/Operatos_CF_Loops/loops.js
+++ Javascript/Basics/Operatos_CF_Loops/loops.js
@@ -1,6 +1,24 @@
-//similar to c++
+//while loop
+while (x < 5){
+    console.log("x is currently: " + x);
+
+    //break
+    if (x===3) {
+        console.log("X IS EQUAL TO THREE");
+        break;
+    }
+
+    console.log("x is still less than 5, adding 1 to x");
+    x = x + 1;
+}
+
+//for loop similar to c++
 for (var i=0; i < 5, i++) {
     console.log("Number is " + i);
 }
 
-//while loop
+var word = "AJHIUHUIH"
+
+for (var i = 0; i < word.length; i++) {
+    console.log(word[i]);
+}
