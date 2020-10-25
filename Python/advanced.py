1c3911b1117c5e134288e5aac1e47419d6197263
Aaron <51930273+Achan40@users.noreply.github.com>
2020-10-25 12:41:44 -0500

    started python section

---
+++ Python/advanced.py
@@ -0,0 +1,17 @@
+# List comprehension basics
+mat = [[1,2,3],[4,5,6],[7,8,9]]
+
+first_col = [i[0] for i in mat]
+print(first_col)
+
+# Dictionaries 
+mystuff = {"key1":123,"key2":'val2',"key3":{'123':[1,2,'grabMe']}}
+print(mystuff['key3']['123'][2])
+
+# Sets : only contain unique elements
+x = set()
+x.add(1)
+x.add(2)
+x.add(2)
+
+print(x)
