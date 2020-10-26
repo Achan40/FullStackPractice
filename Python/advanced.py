1e88c5a63bc7684d47ed8a515a0e77c85f2d16ca
Aaron <51930273+Achan40@users.noreply.github.com>
2020-10-25 20:59:27 -0500

    learning regular expressions

--- Python/advanced.py
+++ Python/advanced.py
@@ -16,3 +16,76 @@ x.add(2)
 
 print(x)
 
+
+# Regular Expressions
+import re
+
+patterns = ["term1", "term2"]
+text = 'This is is a string with term1, not the other'
+
+for pattern in patterns:
+    print("I'm searching for: " + pattern)
+
+    # Using regular expressions to determine if pattern in text, returns match object
+    if re.search(pattern, text):
+        print("Match!")
+    else:
+        print("No Match")
+
+# Returns integer for location of term
+match = re.search('term1',text)
+print(match.start())
+
+# Splitting terms
+split_term = '@'
+email = 'user@gmail.com'
+print(re.split(split_term,email))
+
+# Find all instances of a pattern in a string
+print(re.findall('match', 'test phrase match in middle match'))
+
+def multi_re_find(patterns, phrase):
+    for pat in patterns:
+        print("Searching for pattern {}".format(pat))
+        print(re.findall(pat,phrase))
+        print('\n')
+
+test_phrase = "sdsd..sssddd..sdddsddd...dsds...dsssss...sddddd"
+
+# Want to find an s followed by zero or more d's,
+test_patterns = ['sd*']
+
+multi_re_find(test_patterns,test_phrase)
+
+# Want to find an s followed by one or more d's,
+test_patterns = ['sd+']
+multi_re_find(test_patterns,test_phrase)
+
+# Want to find an s followed by zero or one d's,
+test_patterns = ['sd?']
+multi_re_find(test_patterns,test_phrase)
+
+# Want to find an s followed by n d's,
+test_patterns = ['sd{3}']
+multi_re_find(test_patterns,test_phrase)
+
+# Want to find an s followed by m or n d's,
+test_patterns = ['sd{1,3}']
+multi_re_find(test_patterns,test_phrase)
+
+# Want to find an s followed by one or more s's or one or more d's,
+test_patterns = ['s[sd]+']
+multi_re_find(test_patterns,test_phrase)
+
+# Removing punctuation
+test_phrase = 'This is a string! But it has punctuation, How can we remove it?'
+test_patterns = ['[^!,?]+']
+multi_re_find(test_patterns,test_phrase)
+# There is must more that these as well
+
+# Importing custom modules
+import this_is_a_module
+this_is_a_module.func_in_module()
+
+
+# Decorators
